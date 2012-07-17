b4b1922357c03d7503cf974543b370f83bb7669d
Rich Cox <conejo2005@gmail.com>
2012-07-17 13:24:02 -0700

    initial commit

---
+++ DangerWillRobinson/DangerWillRobinson/Emailer.cs
@@ -0,0 +1,43 @@
+﻿namespace DangerWillRobinson
+{
+	using System.Configuration;
+
+	using FluentEmail;
+
+	public class Emailer
+	{
+		#region Constants and Fields
+
+		private readonly string failCount;
+
+		private readonly string serviceName;
+
+		#endregion
+
+		#region Constructors and Destructors
+
+		public Emailer(string serviceName, string failCount)
+		{
+			this.serviceName = serviceName;
+			this.failCount = failCount;
+		}
+
+		#endregion
+
+		#region Public Methods and Operators
+
+		public void FlapArmsWildly()
+		{
+			string from = ConfigurationManager.AppSettings["from"];
+			string to = ConfigurationManager.AppSettings["notificationList"];
+
+			Email email = Email.From(from)
+				.To(to).Subject(string.Format("{0} failure", this.serviceName))
+				.Body(string.Format("{0} failed to restart. FailCount: {1}", this.serviceName, this.failCount), false);
+
+			email.Send();
+		}
+
+		#endregion
+	}
+}
---
+++ DangerWillRobinson/DangerWillRobinson/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace DangerWillRobinson
+{
+
+	public class Program
+	{
+		#region Methods
+
+		private static void Main(string[] args)
+		{
+			string serviceName = "Default Service Name";
+			string failCount = "Default failCount";
+
+			if (args.Length >= 2)
+			{
+				serviceName = args[0];
+				failCount = args[1];
+			}
+
+			Emailer emailer = new Emailer(serviceName, failCount);
+			emailer.FlapArmsWildly();
+		}
+
+		#endregion
+	}
+}
